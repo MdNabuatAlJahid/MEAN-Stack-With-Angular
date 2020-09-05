e683d4f5ddf46ada1d75ee637358b3a9449f0e6e
Md Nabuat Al Jahid <nabuat.aljahid@gmail.com>
2020-09-05 13:39:56 +0200

    angular added to project and database connection updated

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const mongoose = require("mongoose");
 const config = require("./config/database");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.uri, (err) => {
-  if (err) {
-    console.log("Could not connect to database: ", err);
-  } else {
-    console.log("Connected to database: " + config.db);
+mongoose.connect(
+  config.uri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log("Could not connect to database: ", err);
+    } else {
+      console.log("Connected to database: " + config.db);
+    }
   }
-});
+);
 
 app.get("*", (req, res) => {
   res.send("Hello world");
